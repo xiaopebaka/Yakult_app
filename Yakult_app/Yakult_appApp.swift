f9f3b262f8ffad76f33c664a626702b72aa47eb2
m.kaku <baka9@me.com>
2024-06-26 16:27:15 +0900

    Initial Commit

---
+++ Yakult_app/Yakult_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yakult_appApp.swift
+//  Yakult_app
+//
+//  Created by m.kaku on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Yakult_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
